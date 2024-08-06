0a99365282e71fac74bf253111d225f358c2199d
Julian Florez <forezjuli08@hotmail.com>
2024-08-06 14:45:22 -0500

    add - method implementation for Logger type

--- pocketlog/logger.go
+++ pocketlog/logger.go
@@ -1,5 +1,7 @@
 package pocketlog
 
+import "fmt"
+
 // logger is used to log information
 type Logger struct {
 	threshold Level
@@ -14,15 +16,26 @@ func New(threshold Level) *Logger {
 
 // Debugf formats and prints a message if the log level is debug or higher.
 func (l *Logger) Debugf(format string, args ...any) {
-	// implement me
+	if l.threshold > LevelDebug {
+		return
+	}
+	_, _ = fmt.Printf(format+"\n", args...)
 }
 
 // Infof formats and prints a message if the log level is info or higher.
 func (l *Logger) Infof(format string, args ...any) {
-	// implement me
+	if l.threshold > LevelInfo {
+		return
+	}
+
+	_, _ = fmt.Printf(format+"\n", args...)
 }
 
-// Errorf formats and prints a message if the log level is error.
+// Errorf formats and prints a message if the log level is error or higher.
 func (l *Logger) Errorf(format string, args ...any) {
-	// todo: implement this
+	if l.threshold > LevelError {
+		return
+	}
+
+	_, _ = fmt.Printf(format+"\n", args...)
 }
