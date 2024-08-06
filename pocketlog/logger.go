2b4006a3fd183ccbe56dcf1410f517bfd935b185
Julian Florez <forezjuli08@hotmail.com>
2024-08-06 15:52:11 -0500

    add - output field to Logger struct type (output is of type io.Writer)

--- pocketlog/logger.go
+++ pocketlog/logger.go
@@ -1,41 +1,58 @@
 package pocketlog
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
-// logger is used to log information
+// Logger is used to log information
 type Logger struct {
 	threshold Level
+	output    io.Writer
 }
 
 // New returns a Logger, ready to log at the required threshold.
-func New(threshold Level) *Logger {
+// The default output is Stdout.
+func New(threshold Level, output io.Writer) *Logger {
 	return &Logger{
 		threshold: threshold,
+		output:    output,
 	}
 }
 
 // Debugf formats and prints a message if the log level is debug or higher.
 func (l *Logger) Debugf(format string, args ...any) {
-	if l.threshold > LevelDebug {
-		return
+	// making sure we can safely write to the output
+	if l.output == nil {
+		l.output = os.Stdout
 	}
-	_, _ = fmt.Printf(format+"\n", args...)
+
+	if l.threshold <= LevelDebug {
+		_, _ = fmt.Fprintf(l.output, format+"\n", args...)
+	}
 }
 
 // Infof formats and prints a message if the log level is info or higher.
 func (l *Logger) Infof(format string, args ...any) {
-	if l.threshold > LevelInfo {
-		return
+	// making sure we can safely write to the output
+	if l.output == nil {
+		l.output = os.Stdout
 	}
 
-	_, _ = fmt.Printf(format+"\n", args...)
+	if l.threshold <= LevelDebug {
+		_, _ = fmt.Fprintf(l.output, format+"\n", args...)
+	}
 }
 
 // Errorf formats and prints a message if the log level is error or higher.
 func (l *Logger) Errorf(format string, args ...any) {
-	if l.threshold > LevelError {
-		return
+	// making sure we can safely write to the output
+	if l.output == nil {
+		l.output = os.Stderr
 	}
 
-	_, _ = fmt.Printf(format+"\n", args...)
+	if l.threshold <= LevelDebug {
+		_, _ = fmt.Fprintf(l.output, format+"\n", args...)
+	}
 }
